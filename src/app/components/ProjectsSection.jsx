3f55d4205cdbac6fa4b23f1ccbfe65b1e18651df
SajidHassanS <sajidhassancs@gmail.com>
2024-01-11 00:10:10 +0500

    small view change

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -93,7 +93,7 @@ const ProjectsSection = () => {
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+      <div className="text-white flex flex-wrap justify-center items-center gap-2 py-6 md:flex">
         <ProjectTag
           onClick={handleTagChange}
           name="All"
